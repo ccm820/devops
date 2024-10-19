c9d1d464262dd71cab785f630a567f01c6370f73
caiman <ccm820@163.com>
2024-10-19 13:02:32 +0800

    null judgement

--- CICD/shared_lib/vars/flowBuilder.groovy
+++ CICD/shared_lib/vars/flowBuilder.groovy
@@ -29,9 +29,9 @@ def call() {
                                 echo "Json data is empty"
                             } else {
                                 // 获取某些字段
-                                def ref = JsonPayload.ref // 获取引用（如分支）
-                                def repository = JsonPayload.repository?.name // 获取仓库名
-                                REPO_URL = JsonPayload.repository?.clone_url
+                                def ref = JsonPayload?.ref // 获取引用（如分支）
+                                def repository = JsonPayload?.repository?.name // 获取仓库名
+                                REPO_URL = JsonPayload.?repository?.clone_url
                                 BRANCH_NAME = JsonPayload.ref?.split('/')?.last()
                                 echo "Ref: ${ref}"
                                 echo "REPO_URL: ${repository}"
